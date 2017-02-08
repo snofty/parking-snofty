7701e74e4fe22dd026177f9c000191c9aea4a000
snofty <kunda.sunilkumar@gmail.com>
2017-02-09 03:24:23 +0530

    base code
    display a dynamic table for parking spaces

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-s
+$(document).ready(function(){
+            var container = $('.table-body');
+            var $trs = $();
+    		//var spaces='';
+    		var rows=6;
+    		var columns=3;
+            var i = 1;
+            var index=1;
+    		for (; i <=rows; i++) {
+    			var $tr = $('<tr/>', {class: 'table-row', id: 'table-row-id-'+index});
+    			for (var j = 1; j<=columns; j++) {
+    				//spaces+="<td>Dynamic Space</td>";
+                    $tr.append($('<td />', {class: 'name', id: 'space-'+index, text: 'Space'+index}));
+                    
+                     index++;
+    			}
+    			//spaces+='</tr>';
+                $trs = $trs.add($tr);
+    		}
+ 			//$('#parkingTable').append(spaces);
+            container.append($trs);
+
+            
+            
+		});
+        $(document).on("click", "#parkingTable td", function(e) {
+            alert(this.id);
+        });
+       
